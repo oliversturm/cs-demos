5b0cd7763511e38f9f06d48f70ab891bb4bd1bc7
Oli Sturm <oliver@oliversturm.com>
2022-11-22 22:02:17 +0000

    8.0

---
+++ 8.0/AsyncStreams/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace AsyncStreams {
+  class Program {
+    public static IEnumerable<int> GetInts() {
+      yield return 10;
+      yield return 20;
+      yield return 30;
+    }
+
+    // Asynchronous iterators are just as easy to implement as
+    // synchronous ones -- only they can also await stuff.
+    public static async IAsyncEnumerable<int> GetIntsAsync() {
+      yield return 10;
+      await Task.Delay(1000);
+      yield return 20;
+      await Task.Delay(1000);
+      yield return 30;
+    }
+
+    // Signature of the Main method is async to call await directly
+    static async Task Main(string[] args) {
+      foreach (var i in GetInts()) {
+        Console.WriteLine(i);
+      }
+
+      // await foreach is needed to deal with IAsyncEnumerable<T>
+      await foreach (var i in GetIntsAsync()) {
+        Console.WriteLine(i);
+      }
+    }
+  }
+}
