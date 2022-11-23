c92dbbdb975d138c04fe69ff1e4ebc34fbf099c4
Oli Sturm <oliver@oliversturm.com>
2022-11-23 20:51:39 +0000

    10.0

---
+++ 10.0/RecordTypes/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace RecordTypes {
+  // Here's the product again from C# 9
+  public record Product(string Name, decimal Price);
+
+  // This is the same thing from C# 10 -- `class` is optional
+  public record class Product2(string Name, decimal Price);
+
+  // The new thing is that there are also record structs.
+  // For a laugh, positional properties in record structs are
+  // mutable by default.
+  public record struct ProductStruct(string Name, decimal Price);
+
+  // To make a record struct immutable, `readonly` can be added
+  public readonly record struct ProductStructReadonly(string Name, decimal Price) {
+    // Adding properties is allowed, but they must also be readonly
+    // This does not work
+    // public string Extra { get; set; }
+  }
+
+  class Program {
+    static void Main(string[] args) {
+      var p1 = new Product("Rubber Chicken", 13.99m);
+      var p2 = new Product2("Rubber Chicken", 13.99m);
+
+      var ps = new ProductStruct("Rubber Chicken", 13.99m);
+      ps.Name = "Broken Rubber Chicken";
+
+      var psr = new ProductStructReadonly("Rubber Chicken", 13.99m);
+      //psr.Name = "Broken Rubber Chicken";
+
+      // Rendering works as before
+      Console.WriteLine(p1);
+      Console.WriteLine(p2);
+      Console.WriteLine(ps);
+
+      // Equality works the same as with record classes.
+      // https://sharplab.io tells me that record structs also have a 
+      // compiler-generated equality implementation and do not rely on
+      // struct based value equality or reflection.
+      var p1_ = new Product("Rubber Chicken", 13.99m);
+      Console.WriteLine($"Reference equals: {Object.ReferenceEquals(p1, p1_)}");
+      Console.WriteLine($"Value equality: {p1 == p1_}");
+
+    }
+  }
+}
---
+++ 10.0/WithNews/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace WithNews {
+  public record SizeRecord(int Width, int Height);
+
+  public struct SizeStruct {
+    public int Width { get; init; }
+    public int Height { get; init; }
+
+    public override string ToString() => $"SizeStruct {{ Width = {Width}, Height = {Height} }}";
+  }
+
+  class Program {
+    static void Main(string[] args) {
+      // Since C# 9 we can use `with` with records
+      var sr1 = new SizeRecord(10, 10);
+      var sr2 = sr1 with { Width = 20 };
+      Console.WriteLine(sr1);
+      Console.WriteLine(sr2);
+
+      // In C# 10, structs also support `with`
+      var ss1 = new SizeStruct { Width = 10, Height = 10 };
+      var ss2 = ss1 with { Width = 20 };
+      Console.WriteLine(ss1);
+      Console.WriteLine(ss2);
+
+      // Additionally, `with` works with anonymous types
+      var a1 = new { Width = 10, Height = 10 };
+      var a2 = a1 with { Width = 20 };
+      Console.WriteLine(a1);
+      Console.WriteLine(a2);
+    }
+  }
+}
