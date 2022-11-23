73e9e0173cb5cbf5a64e958cec6db7d94f8ff75c
Oli Sturm <oliver@oliversturm.com>
2022-11-23 21:42:33 +0000

    10.0

---
+++ 10.0/CallerArgumentExpressions/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace CallerArgumentExpressions {
+  class Program {
+
+    // The compiler creates the string passed to `messageThat`, in a 
+    // form that would be impossible to retrieve at runtime.
+    static void Assert(bool that,
+      [CallerArgumentExpression("that")] string? messageThat = default) {
+      if (!that)
+        throw new Exception($"Turns out that's untrue: {messageThat}");
+    }
+
+    // Even a somewhat complex parameter can be formatted
+    static void Assert(Func<bool> checker,
+      [CallerArgumentExpression("checker")] string? messageChecker = default) {
+      if (!checker())
+        throw new Exception($"Turns out that's untrue: {messageChecker}");
+    }
+
+    static void Main(string[] args) {
+      Assert(100 == 101);
+      //Assert(() => 100 == 101);
+    }
+  }
+}
