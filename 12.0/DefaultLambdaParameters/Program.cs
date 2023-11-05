5b8215e48e0b8b6fe58a24d8a8be6cb24efda996
Oli Sturm <oliver+commitsigning@oliversturm.com>
2023-11-05 23:15:28 +0000

    feat: default lambda parameters

---
+++ 12.0/DefaultLambdaParameters/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace DefaultLambdaParameters {
+  static class Program {
+    static void Main(string[] args) {
+      // Before C# 12, lambda expressions could not have default parameters.
+      var power = (int val, int exp = 2) => Math.Pow(val, exp);
+
+      Console.WriteLine(power(2));
+      Console.WriteLine(power(2, 3));
+      Console.WriteLine(power(2, 8));
+      Console.WriteLine(power(2, 16));
+    }
+  }
+}
