a518f7225e0fcf22274395b0bd1c9d86ca70a069
Oli Sturm <oliver+commitsigning@oliversturm.com>
2023-11-05 22:40:55 +0000

    feat: collection expressions

---
+++ 12.0/CollectionExpressions/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace CollectionExpressions {
+  static class Program {
+    static void Main(string[] args) {
+      // int[] can be instantiated and filled this way, but
+      // type inference doesn't work
+      int[] odds = [1, 3, 5, 7, 9, 11];
+
+      // One reason is that multiple target types are supported
+      Span<int> evens = [2, 4, 6, 8, 10, 12];
+      List<int> primes = [2, 3, 5, 7, 11, 13];
+
+      // Multiple dimensions are supported
+      int[][] matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+
+      // The spread operator can be used
+      int[] allNumbers = [..evens, ..odds];
+
+      IEnumerable<int> OnlyByFives(IEnumerable<int> source) {
+        foreach (var item in source) {
+          if (item % 5 == 0) yield return item;
+        }
+      }
+
+      // A collection expression can also be passed to a method that
+      // expects one.
+      int[] primesAndByFives = [..primes, ..OnlyByFives([..evens, ..odds])];
+
+      foreach (int i in primesAndByFives) Console.WriteLine(i);
+    }
+  }
+}
