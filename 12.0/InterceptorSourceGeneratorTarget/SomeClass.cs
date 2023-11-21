4e563f272ad223205e49be8fde7b3d4a19f50f2a
Oli Sturm <oliver+commitsigning@oliversturm.com>
2023-11-21 17:17:44 +0000

    feat: interceptor source generator (deactivated by default)

--- 12.0/InterceptorSourceGeneratorTarget/SomeClass.cs
+++ 12.0/InterceptorSourceGeneratorTarget/SomeClass.cs
@@ -3,7 +3,7 @@ using System;
 namespace InterceptorSourceGeneratorTarget;
 
 public class SomeClass {
-    public void DoSomething(int arg) {
-        Console.WriteLine($"Doing something with {arg}");
-    }
+  public void DoSomething(int arg) {
+    Console.WriteLine($"Doing something with {arg}");
+  }
 }
