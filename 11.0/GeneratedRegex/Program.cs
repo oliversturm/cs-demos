aee2bca1862363340759165b797965a1876aac1c
Oli Sturm <oliver+commitsigning@oliversturm.com>
2023-11-25 23:04:40 +0000

    feat: generated regex

---
+++ 11.0/GeneratedRegex/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.RegularExpressions;
+
+public static partial class Program {
+  [GeneratedRegex(@"[abc]+")]
+  private static partial Regex MyRegex();
+
+  public static void Main(string[] args) {
+    Console.WriteLine("Hello World!");
+  }
+}
