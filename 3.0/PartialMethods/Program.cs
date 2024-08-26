306e3b6f5e44873f040e669d94968c873e1915ba
Oli Sturm <oliver+commitsigning@oliversturm.com>
2024-08-26 22:46:36 +0100

    adding details

--- 3.0/PartialMethods/Program.cs
+++ 3.0/PartialMethods/Program.cs
@@ -1,37 +1,39 @@
 ﻿// Copyright 2010 Oliver Sturm <oliver@sturmnet.org> All rights reserved. 
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+namespace PartialMethods;
 
-namespace PartialMethods {
-  class Program {
-    static void Main(string[] args) {
-      var generated = new Generated();
-      generated.DoSomething();
-    }
+internal class Program {
+  private static void Main(string[] args) {
+    Generated generated = new Generated();
+    generated.DoSomething();
+  }
+}
+
+internal partial class Generated {
+  // If this implementation is commented out, the compiler
+  // gets rid of the call to PartialMethod() in the
+  // Generated.DoSomething method. You can verify this by
+  // looking at Reflector.
+
+  partial void PartialMethod() {
+    Console.WriteLine("Partial Method executed");
+  }
+}
+
+internal partial class Generated {
+  private int generatedField;
+
+  public void DoSomething() {
+    generatedField = 10;
+    PartialMethod();
   }
 
-  partial class Generated {
-    // If this implementation is commented out, the compiler
-    // gets rid of the call to PartialMethod() in the
-    // Generated.DoSomething method. You can verify this by
-    // looking at Reflector.
+  partial void PartialMethod();
 
-    partial void PartialMethod() {
-      Console.WriteLine("Partial Method executed");
-    }
-  }
+  // This method is not implemented and it will be removed.
+  // Note that it must use the default visibility -- even
+  // the use of the "private" keyword will make the compiler complain.
+  partial void AnotherPartialMethod();
 
-  partial class Generated {
-    private int generatedField;
-
-    public void DoSomething() {
-      generatedField = 10;
-      PartialMethod();
-    }
-
-    partial void PartialMethod();
-  }
+  //private partial void YetAnotherPartialMethod();
 }
